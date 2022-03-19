6829a9fb59927c943149ce1b791f34638eae217d
danolaru <dan.olaru@tokinomo.com>
2022-03-19 14:00:04 +0100

    added a route

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,59 @@
 var express = require('express');
 var router = express.Router();
 
+const mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'dannyo',
+  password: 'dannyo',
+  database: 'longmoneyOffshore'
+});
+
+connection.connect();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/hello', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Hello World X' });
+  console.log("HELLO WORLD endpoint was accessed");
+
+});
+
+router.post('/saveUser', function(req, res, next) {
+  // res.render('index', { title: 'Hello World X' });
+
+  console.log("SAVE USER endpoint accessed ");
+
+  let bod = req.body;
+
+  console.log("bodie " + JSON.stringify(bod));
+  console.log(bod);
+
+  let payload = JSON.parse(JSON.stringify(bod));
+
+  // res.send(payload);
+  // res.send("GET FUGGD");
+
+  let q = 'INSERT INTO `entry` (`id`, `date`, `name`) VALUES (' +
+    payload.id + ', "' +
+    payload.date + '", "' +
+    payload.name + '");';
+
+  console.log('The QUERY HOT RELOAD: ', q);
+
+
+  connection.query(q, (err, rows, fields) => {
+    if (err) throw err;
+    //console.log('The solution is: ', JSON.stringify(rows));
+
+    res.send('INSERTED ' + JSON.stringify(payload) + " INTO DB.");
+
+  });
+
 });
 
 module.exports = router;
